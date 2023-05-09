1ab71da6726964d1a720007ba482eb012c0252c1
banggu0321 <99bang@naver.com>
2023-05-09 09:47:10 +0900

    0508

---
+++ ux400_solving/webapp/controller/Main.controller.js
@@ -0,0 +1,107 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller, JSONModel) {
+        "use strict";
+
+        return Controller.extend("sap.btp.ux400solving.controller.Main", {
+            formatter:{
+                transformDiscontinued : function(sData){
+                    // console.log(sData);
+                    // if (sData == true){
+                    //     sData = "Yes";
+                    // }else{
+                    //     sData = "No";
+                    // }    
+                    // return sData;
+                    return sData == true ? 'Yes' : 'No'; 
+                }
+            },
+            onInit: function () {
+                // var datas = {
+                //     randomValue : [
+                //     ]
+                // };
+
+                this.getView().setModel(new JSONModel({value:[]}),"list");
+            },
+            onRandomPress: function(){
+                var oControl = this.byId("idInput");
+
+                oControl.setValueState("None");
+
+                var oListModel = this.getView().getModel("list"),
+                    aList = oListModel.getData().value;
+
+
+                // if(oControl.getValue() == ""){
+                    var cRandomValue = Math.floor(Math.random() * 100) + 1 ;
+                    oControl.setValue(cRandomValue);
+                    
+                    var result = {num : cRandomValue};
+                    aList.push(result);
+                    oListModel.setProperty("/value", aList);
+                // }else{
+                //     var result = {num : oControl.getValue()};
+                //     aList.push(result);
+                //     oListModel.setProperty("/value", aList);
+                // }
+            },
+            onOpenProduct: function(){
+                var oDialog = this.byId("ProductsDialog"); //DialogID
+
+                if (oDialog){
+                    oDialog.open();
+                    return;                    
+                }
+                
+                this.loadFragment({
+                    name: "sap.btp.ux400solving.view.fragment.Products"
+                }).then(function(oDialog){
+                    oDialog.open();
+                    // debugger;
+                },this);
+
+                // debugger;
+            },
+            onClose: function(oEvent){
+                var oDialog = oEvent.getSource().getParent(); //Dialog        
+                // var oDialog = this.byId("ProductsDialog");      
+                oDialog.close();
+            },
+            onValueChange: function(){
+                var sInputValue = Number(this.byId("idInput").getValue());
+                // var sInputValue2 = this.byId("idInput").getValue();
+                // debugger;
+
+                if(sInputValue >= 1 && sInputValue <= 100 ){
+                    this.byId("idInput").setValueState("None");
+
+                    var oListModel = this.getView().getModel("list"),
+                        aList = oListModel.getData().value;
+                    
+                    var result = {num : sInputValue};
+                    aList.push(result);
+                    oListModel.setProperty("/value", aList);
+                    this.byId("idBtn").setEnabled(true);
+                }else if(sInputValue = ""){
+                    this.byId("idInput").setValueState("None");
+                }else{
+                    this.byId("idInput").setValueState("Error");
+                    // this.byId("idInput").setValueStateText("1이상 100이하의 숫자를 입력하세요");
+                    this.byId("idBtn").setEnabled(false);
+                }
+
+                // let isOK = iNum >= 1 && iNum <= 100;
+
+                // oControl.setValueState(isOK?'None':'Error');
+                // oControl.setValueStatText(isOK?'':'1~100숫자를 입력하세요');
+                // this.byId("idBtn").setEnabled(isOK?true:false);
+
+            }
+        });
+    });
---
+++ zproject0504/README.md
@@ -0,0 +1,43 @@
+## Application Details
+|               |
+| ------------- |
+|**Generation Date and Time**<br>Thu May 04 2023 18:01:21 GMT+0900 (Korean Standard Time)|
+|**App Generator**<br>@sap/generator-fiori-elements|
+|**App Generator Version**<br>1.9.4|
+|**Generation Platform**<br>Visual Studio Code|
+|**Template Used**<br>Overview Page V2|
+|**Service Type**<br>OData Url|
+|**Service URL**<br>https://services.odata.org/v2/northwind/northwind.svc/
+|**Module Name**<br>zproject0504|
+|**Application Title**<br>App Title|
+|**Namespace**<br>|
+|**UI5 Theme**<br>sap_horizon|
+|**UI5 Version**<br>1.113.0|
+|**Enable Code Assist Libraries**<br>False|
+|**Enable TypeScript**<br>False|
+|**Add Eslint configuration**<br>False|
+|**FilterEntity Type**<br>Customer|
+
+## zproject0504
+
+A Fiori application.
+
+### Starting the generated app
+
+-   This app has been generated using the SAP Fiori tools - App Generator, as part of the SAP Fiori tools suite.  In order to launch the generated app, simply run the following from the generated app root folder:
+
+```
+    npm start
+```
+
+- It is also possible to run the application using mock data that reflects the OData Service URL supplied during application generation.  In order to run the application with Mock Data, run the following from the generated app root folder:
+
+```
+    npm run start-mock
+```
+
+#### Pre-requisites:
+
+1. Active NodeJS LTS (Long Term Support) version and associated supported NPM version.  (See https://nodejs.org)
+
+
