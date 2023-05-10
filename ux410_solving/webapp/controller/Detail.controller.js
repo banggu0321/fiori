d957250f192ccfb979e70cbb21edb1e765b1cd3a
banggu0321 <99bang@naver.com>
2023-05-10 10:30:30 +0900

    5.10 (5.9리뷰)

--- ux410_solving/webapp/controller/Detail.controller.js
+++ ux410_solving/webapp/controller/Detail.controller.js
@@ -8,11 +8,8 @@ sap.ui.define(
   
       return BaseController.extend("sap.btp.ux410solving.controller.Detail", {
         onInit() {
-            this.getView().setModel(new JSONModel(),'DetailModel');
-            
             var oRouter = this.getOwnerComponent().getRouter();
             oRouter.getRoute("RouteDetail").attachPatternMatched(this._onPatternMatched, this);
-
         },
         _onPatternMatched : function(oEvent){
             
@@ -27,10 +24,20 @@ sap.ui.define(
             this.getView().bindElement(sBindPath); //View에다가 해당 데이터 세팅
             //위 Binding 방법을 Element Binding 또는 Context Binding 이라고 부름
 
-
             // sBindPath = `/Order_Details(OrderID=${oArgu.OrderID},ProductID=${oArgu.ProductID})`;
-
             // this.getView().bindElement("/Order_Details(OrderID="+oArgu.OrderID+",ProductID="+oArgu.ProductID+")");
+
+            //서버에서 전체 데이터 가져옴
+            this.getView().getModel().read("/Order_Details",{
+              success: function(oRetrun){
+                debugger ; //500건
+                // oRetrun 안 조회 데이터가 들어오게 됨
+                // 해당 데이터를 가지고 데이터 가공을 할 수 있음
+                
+                //여기에서 데이터 받아와서 데이터 핸들링! 
+              }
+            });
+            //데이터 핸들링 하면 오류 발생XXXXXXX
         }
       });
     }
