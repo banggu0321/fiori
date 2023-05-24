abdf987f9b9287b1d74139c07aed51b337acadb1
banggu0321 <99bang@naver.com>
2023-05-24 18:31:40 +0900

    5.24 binding error

--- zfer_slipchart/webapp/controller/Main.controller.js
+++ zfer_slipchart/webapp/controller/Main.controller.js
@@ -12,19 +12,34 @@ sap.ui.define([
             onInit: function () {
                 // oModel.loadData( sap.ui.require.toUrl("zprojectteste1104/model/data.json") );
                 // this.getView().setModel(new JSONModel(),"PartnerList");
-                this.getView().setModel(new JSONModel(sap.ui.require.toUrl("ER/zferslipchart/model/Tree.json")),"PartnerList");
+                // this.getView().setModel(new JSONModel(sap.ui.require.toUrl("ER/zferslipchart/model/Tree.json")),"PartnerList");
                 
-                
+                this.getView().setModel(new JSONModel(),"slipAll");
+                this.getView().setModel(new JSONModel(),"partnerList");
+                this.getView().setModel(new JSONModel(),"slipPartnerList");
+
+                this._defaultSet();
                 this._getData();
                 // debugger;
                 // var oModel = new JSONModel();
                 // oModel.loadData( sap.ui.require.toUrl("ER.zferslipchart/webapp/model/Tree.json") );
                 // this.getView().setModel(oModel, 'ParterList');
             },
+            _defaultSet : function(){
+                // odata model 변수 세팅
+                this.oModel = this.getOwnerComponent().getModel(); //oData
+                //json model 변수 세팅
+                this.oslipAll = this.getView().getModel("slipAll"); //slipAll
+                //json model 변수 세팅
+                this.opartnerList = this.getView().getModel("partnerList"); //partneList
+                //json model 변수 세팅
+                this.oslipPartnerList = this.getView().getModel("slipPartnerList"); //partneList
+            },
             _getData : function(){
-                // var oPartnerList = { "list":[ ]};
                 var aPartner = [];
                 var aPartnercode = [];
+                var aPartnerAll = [];
+                var aPartnerAmtTem = [];
                 this.getOwnerComponent().getModel().read("/employeeSet", {
                     success: function(oReturn) {
                         this.empid = oReturn.results[0].Employeeid;
@@ -36,8 +51,9 @@ sap.ui.define([
 
                 this.getOwnerComponent().getModel().read("/partnerSet", {
                     success: function(oReturn) {
-                        debugger;
-                        for(var i = 0; i < oReturn.results.length + 1 ; i++){
+                        this.opartnerList.setProperty("/list",oReturn);
+                        for(var i = 0; i < oReturn.results.length + 1 ; i++){ //0 6
+                            // debugger;
                             var j = i - 1;
                             if(i === oReturn.results.length){
                                 aPartnercode.push({text: oReturn.results[j].Partcode,nodes:aPartner});
@@ -50,13 +66,104 @@ sap.ui.define([
                                 aPartner = [];
                                 aPartner.push({text: oReturn.results[i].Partid});
                             }
+
+                            if(oReturn.results[i] != undefined){
+                                aPartnerAll.push(oReturn.results[i].Partid);
+                                aPartnerAmtTem.push(0);
+                            }
                         }
-                        this.getView().setModel(new JSONModel({list:aPartnercode}),"PartnerList");
-                        debugger;
+                        this.getView().setModel(new JSONModel({list:aPartnercode}),"PartnerTreeList");
                     }.bind(this)
                 });
 
-                // this.getOwnerComponent().getModel().read("/partnercodeSet", {
+                this.getOwnerComponent().getModel().read("/sliphSet", {
+                    success: function(oReturn) {
+                        // var sPstdatei, sPstdatej ;
+                        var aPartnerAmt = aPartnerAmtTem;
+                        var aDatas = [];
+                        // debugger;
+                        for(var i = 0; i < oReturn.results.length  ; i++){
+                            // debugger;
+                            var sPstdatei, sPstdatej;
+                            var j = i + 1;
+                            sPstdatei = this._datefomatter(oReturn.results[i].Pstdate); //202304 , 202305
+                            
+                            
+                            if(i != oReturn.results.length - 1){
+                                sPstdatej = this._datefomatter(oReturn.results[j].Pstdate); //202305, 202306
+                            };
+                            // debugger;
+                            
+                            if(sPstdatei === sPstdatej){
+                                for(var k = 0; k < aPartnerAll.length ; k++){
+                                    if(aPartnerAll[k] === oReturn.results[i].Partid){
+                                        aPartnerAmt[k] += Number(oReturn.results[i].Amt);
+                                        break;
+                                    }
+                                }
+                                // debugger;
+                            }
+                            // else if( i === oReturn.results.length - 1){
+                            //     for(var l = 0; l < aPartnerAll.length ; l++){
+                            //         if(aPartnerAll[l] === oReturn.results[i].Partid){
+                            //             aPartnerAmt[l] += Number(oReturn.results[i].Amt);
+                            //             break;
+                            //         }
+                            //     }
+                            //     // debugger;
+                            // }
+                            else {
+                                for(var l = 0; l < aPartnerAll.length ; l++){
+                                    if(aPartnerAll[l] === oReturn.results[i].Partid){
+                                        aPartnerAmt[l] += Number(oReturn.results[i].Amt);
+                                        break;
+                                    }
+                                };
+                                var oData = {
+                                    year: sPstdatei.substr(0, 4), //20230401 ->2023
+                                    month: oReturn.results[i].Month              //'1'
+                                  };
+
+                                for (var m = 0; m < aPartnerAll.length; m++) {
+                                    var key = aPartnerAll[m];
+                                    var value = aPartnerAmt[m];
+                                    oData[key] = value;
+                                }
+                                aDatas.push(oData);
+                                aPartnerAmt = aPartnerAmtTem;
+                                // debugger;
+                            };
+
+                            //무한루프
+                            if(i === oReturn.results.length - 1){
+                                var oData = {
+                                    year: sPstdatei.substr(0, 4), //20230401 ->2023
+                                    month: oReturn.results[i].Month              //'1'
+                                  };
+
+                                for (var o = 0; o < aPartnerAll.length; o++) {
+                                    var key = aPartnerAll[o];
+                                    var value = aPartnerAmt[o];
+                                    oData[key] = value;
+                                }
+                                aDatas.push(oData);
+                                aPartnerAmt = aPartnerAmtTem;
+                            };
+                        };
+                        this.oslipPartnerList.setProperty("/list",aDatas);
+                    
+                        
+                        // debugger;
+
+                        // debugger;
+
+                        // aPartnerAmt = [0, 0, 409500, 3842370, 0] ;  
+                        // aPartnerAll = ['PAT05', 'PAT00', 'PAT07', 'PAT08', 'PAT09'];
+                        // oData = {PAT05:0, PAT00:0, PAT07:409500, PAT08:3842370, PAT09:0};
+                        // debugger;
+                    }.bind(this)
+                });
+                // this.getOwnerComponent().getModel().read("/sliphSet", {
                 //     success: function(oReturn) {
                 //         for(var j = 0; j < oReturn.results.length; j++){
                 //             oPartnerList.push
@@ -65,6 +172,19 @@ sap.ui.define([
                 //     }.bind(this)
                 // });
                 
+            },
+            _datefomatter: function(date){
+
+                let oDateTimeInstance;
+
+                oDateTimeInstance = sap.ui.core.format.DateFormat.getDateTimeInstance({
+                    pattern : 'yyyyMM'
+                });
+
+                return oDateTimeInstance.format(date);
+            },
+            onSearch : function(){
+                // debugger;
             }
         });
     });
--- zfer_slipchart/webapp/model/Tree2.js
+++ zfer_slipchart/webapp/model/Tree2.js
@@ -6,3 +6,20 @@
         {text: "m",nodes: [{text: "Node1-1",nodes:[]}]}
     ]       
 }
+
+{
+    list:[
+        {year: '2023' ,month: "1", part01:"b", part02:"b", part03:"b", part04:"b", part05:"b"},
+        {year: '2023' ,month: "2", part01:"b", part02:"b", part03:"b", part04:"b", part05:"b"},
+        {year: '2023' ,month: "3", part01:"b", part02:"b", part03:"b", part04:"b", part05:"b"},
+        {year: '2023' ,month: "4", part01:"b", part02:"b", part03:"b", part04:"b", part05:"b"},
+        {year: '2023' ,month: "5", part01:"b", part02:"b", part03:"b", part04:"b", part05:"b"},
+        {year: '2023' ,month: "6", part01:"b", part02:"b", part03:"b", part04:"b", part05:"b"},
+        {year: '2023' ,month: "7", part01:"b", part02:"b", part03:"b", part04:"b", part05:"b"},
+        {year: '2023' ,month: "8", part01:"b", part02:"b", part03:"b", part04:"b", part05:"b"},
+        {year: '2023' ,month: "9", part01:"b", part02:"b", part03:"b", part04:"b", part05:"b"},
+        {year: '2023' ,month: "10", part01:"b", part02:"b", part03:"b", part04:"b", part05:"b"},
+        {year: '2023' ,month: "11", part01:"b", part02:"b", part03:"b", part04:"b", part05:"b"},
+        {year: '2023' ,month: "12", part01:"b", part02:"b", part03:"b", part04:"b", part05:"b"}
+    ]       
+}
